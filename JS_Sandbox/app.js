0318c1895c158b6d3b737edf6eda8d1ad3c7d061
Paul Tobin <tobster111@gmail.com>
2020-07-13 15:20:45 +0100

    Example of primative and reference types

---
+++ JS_Sandbox/app.js
@@ -0,0 +1,23 @@
+// PRIMATIVE
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null;
+// Undefined
+let test = undefined;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+  city: 'Boston',
+  state: 'MA',
+};
