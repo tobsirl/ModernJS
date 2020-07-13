0360d1c6721556b55ee536af06e3fa792014da04
Paul Tobin <tobster111@gmail.com>
2020-07-13 15:43:14 +0100

    Type conversion

--- JS_Sandbox/app.js
+++ JS_Sandbox/app.js
@@ -21,3 +21,35 @@ const address = {
   city: 'Boston',
   state: 'MA',
 };
+
+let val;
+
+// Number to string
+val = String(5);
+// Bool to a string
+val = String(true);
+// Date to a string
+val = String(new Date());
+// Array to string
+val = String([1, 2, 3, 4]);
+
+// toString()
+val = (5).toString();
+val = true.toString();
+
+// String to number
+val = Number('5');
+val = Number(true);
+val = Number(false);
+val = Number(null);
+val = Number('hello');
+val = Number([1, 2, 3, 4]);
+
+val = parseInt('100.30');
+val = parseFloat('100.30');
+
+// output
+console.log(val);
+console.log(typeof val);
+// console.log(val.length);
+console.log(val.toFixed(2));
